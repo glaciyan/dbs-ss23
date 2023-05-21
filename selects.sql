529ad96919f9eccf1d9494bb79a2415a5caad253
Kevin T <30321316+glaciyan@users.noreply.github.com>
2023-05-21 15:24:29 +0200

    task numbers

--- selects.sql
+++ selects.sql
@@ -1,9 +1,9 @@
--- Welche Belegungen gibt es für eine gegebene Ferienwohnung?
+-- 1) Welche Belegungen gibt es für eine gegebene Ferienwohnung?
 SELECT *
 FROM BELEGUNG
 WHERE FERIENWOHNUNG_ID = ?;
 
--- Welche Personen haben eine gegebene Ferienwohnung reserviert? Personen, die diese Ferienwohnung
+-- 2) Welche Personen haben eine gegebene Ferienwohnung reserviert? Personen, die diese Ferienwohnung
 -- mehrmals reserviert haben, sollen dabei nur einmal ausgegeben werden.
 SELECT DISTINCT K.*
 FROM BELEGUNG
@@ -11,21 +11,22 @@ FROM BELEGUNG
 WHERE FERIENWOHNUNG_ID = ?
   AND BELEGUNG.STATUS_FLAG = 'reservierung';
 
--- Wie viele Buchungen hat eine Person mit einer gegebenen Kundenummer getätigt?
+-- 3) Wie viele Buchungen hat eine Person mit einer gegebenen Kundenummer getätigt?
 SELECT COUNT(*)
 FROM BELEGUNG
 WHERE KUNDE_ID = ?
   AND STATUS_FLAG = 'buchung';
 
--- Wie viele Buchungen haben Personen mit einem gegebenen Nachnamen getätigt? Dabei sollen nur Per-
+-- 4) Wie viele Buchungen haben Personen mit einem gegebenen Nachnamen getätigt? Dabei sollen nur Per-
 -- sonen ausgegeben werden, die mindestens eine Buchung getätigt haben. Beachten Sie weiterhin, dass es
 -- mehrere Personen mit dem gleichem Nachnamen geben kann.
 SELECT DISTINCT K.VORNAME, K.NAME, (SELECT COUNT(*) FROM BELEGUNG WHERE K.KUNDENNUMMER = BELEGUNG.KUNDE_ID)
 FROM BELEGUNG
          INNER JOIN KUNDE K on K.KUNDENNUMMER = BELEGUNG.KUNDE_ID
-WHERE K.NAME = ? AND STATUS_FLAG = 'buchung';
+WHERE K.NAME = ?
+  AND STATUS_FLAG = 'buchung';
 
--- Welche Ferienwohnungen in Frankreich sind höchstens 100 km von (dem Ort) der Touristenattraktion
+-- 5) Welche Ferienwohnungen in Frankreich sind höchstens 100 km von (dem Ort) der Touristenattraktion
 -- Disneyland entfernt?
 SELECT F.*, T.NAME, ENTFERNUNG_KM
 FROM ADRESSEENTFERNUNG
